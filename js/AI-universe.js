e23a81bfcd07de3d1f217287e89752e968a6711f
aalnoman10 <aalnomanm10@gmail.com>
2023-03-03 11:25:51 +0600

    simple modal complate

--- js/AI-universe.js
+++ js/AI-universe.js
@@ -24,7 +24,7 @@ const displayDataItems = (items) => {
                         <h2>${item.name}</h2>
                         <p><i class="fa-solid fa-calendar-days"></i></i> ${item.published_in}</p>
                     </div>
-                    <button class="btn btn-secondary rounded-5 ms-auto"><i class="fa-solid fa-arrow-right"></i></button>
+                    <button onclick="aiDetailsLoading('${item.id}')" type="button" class="btn btn-secondary rounded-5 ms-auto" data-bs-toggle="modal" data-bs-target="#ai-details"><i class="fa-solid fa-arrow-right"></i></button>
                 </div>
             </div>
         `
@@ -32,6 +32,58 @@ const displayDataItems = (items) => {
 
         // const listContainers = document.getElementsByClassName("list-container");
     });
+}
+
+const aiDetailsLoading = async (id) => {
+    const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(URL);
+    const data = await res.json();
+    aiDetailsDisplay(data.data)
+}
+
+const aiDetailsDisplay = (details) => {
+    const modal = document.getElementById("modal");
+    console.log(details);
+    modal.innerHTML = `
+    <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 p-5 g-2">
+        <div class="col">
+            <div class="card h-100 p-3">
+                <h3>${details.description}</h3>
+                <div class="row row-cols-3">
+                    <div class="col">
+                        <button class="btn btn-danger w-100 py-3">${details}</button>
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-danger w-100 py-3">${details}</button>
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-danger w-100 py-3">${details}</button>
+                    </div>
+                </div>
+                <div class="row row-cols-2">
+                    <div class="col">
+                        <ul></ul>
+                    </div>
+                    <div class="col">
+                        <ul></ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100 p-3">
+                <img src="${details.image_link[0]}" class="card-img-top h-100" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${details.input_output_examples[0]?.input}</h5>
+                    <p>${details.input_output_examples[0]?.output}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
 }
 
 loadingAi();
